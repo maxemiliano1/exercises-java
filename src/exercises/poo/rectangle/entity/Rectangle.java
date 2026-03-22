07c8c3113d0a86a2265960a462f7e9a22d154925
max <maxemilio13@hotmail.com>
2026-03-22 19:12:34 -0300

    add exercice POO

---
+++ src/exercises/poo/rectangle/entity/Rectangle.java
@@ -0,0 +1,18 @@
+package exercises.poo.rectangle.entity;
+
+public class Rectangle {
+    public double width, height;
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt((width * width) + (height * height));
+    }
+
+}
